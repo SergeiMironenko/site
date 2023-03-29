bb6aa68332303e3b32033b7eba30cd0deb0a3716
user <email>
2023-03-29 21:32:05 +0700

    update Screen1 (add 100vh...)

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -10,8 +10,7 @@ import Paper from '@mui/material/Paper';
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
-import FormGroup from '@mui/material/FormGroup';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Select from '@mui/material/Select';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import Stack from '@mui/material/Stack';
@@ -35,32 +34,44 @@ function Welcome() {
   return <div><Screen1 /></div>
 }
 
-function TableRowFun(props) {
+function RenderTableRow(props) {
   return (
     <TableRow>
-      <TableCellFun hide={false} value={props.value['name']} />
-      <TableCellFun hide={props.hide} value={props.value['gender']} />
-      <TableCellFun hide={props.hide} value={props.value['dir']} />
-      <TableCellFun hide={props.hide} value={props.value['teacher']} />
-      <TableCellFun hide={false} value={props.value['jump']} />
-      <TableCellFun hide={false} value={props.value['up']} />
-      <TableCellFun hide={false} value={props.value['pushup']} />
-      <TableCellFun hide={false} value={props.value['flex']} />
-      <TableCellFun hide={false} value={props.value['run']} />
-      <TableCellFun hide={false} value={props.value['height']} />
-      <TableCellFun hide={false} value={props.value['weight']} />
-      <TableCellFun hide={false} value={props.value['I']} />
+      <RenderTableCell value={props.value['name']} />
+      <RenderTableCell hide={props.hide} value={props.value['gender']} />
+      <RenderTableCell edit hide={props.hide} value={props.value['dir']} />
+      <RenderTableCell edit hide={props.hide} value={props.value['teacher']} />
+      <RenderTableCell edit value={props.value['jump']} />
+      <RenderTableCell edit value={props.value['up']} />
+      <RenderTableCell edit value={props.value['pushup']} />
+      <RenderTableCell edit value={props.value['flex']} />
+      <RenderTableCell edit value={props.value['run']} />
+      <RenderTableCell edit value={props.value['height']} />
+      <RenderTableCell edit value={props.value['weight']} />
+      <RenderTableCell value={props.value['I']} />
     </TableRow>);
 }
 
-function TableCellFun(props) {
+function RenderTableCell(props) {
   if (props.hide) return null;
+  var elem = "";
+  if (props.edit)
+    elem = <TextField
+      defaultValue={props.value}
+      variant="standard"
+      multiline
+    />;
+  else
+    elem = props.value;
   return <TableCell
     className={props.className}
     rowSpan={props.rowSpan}
     colSpan={props.colSpan}
-  >{props.value}</TableCell>;
-
+    style={props.style}
+    autoWidth
+  >
+    {elem}
+  </TableCell>;
 }
 
 class Screen1 extends React.Component {
@@ -70,34 +81,11 @@ class Screen1 extends React.Component {
     this.state = {
       isOnlyNormatives: false,
       listItems: '',
-      list: Array(15).fill(0).map((item, i) => ({
-        name: ("Student " + i),
-        gender: i % 2 ? "M" : "Ж",
-        dir: i % 3 === 0 ? "Легкая атлетика" : "Н/Д",
-        teacher: i % 4 === 0 ? "TheBest" : "Н/Д",
-        jump: 0,
-        up: 0,
-        pushup: 0,
-        flex: 0,
-        run: 0,
-        height: 0,
-        weight: 0,
-        I: "00.00",
-      })),
       year: 2022,
       semester: "Весенний",
       faculty: "ФПМИ",
-      faculties: [
-        "ФПМИ",
-        "ФЛА",
-        "АВТФ"
-      ],
       course: 1,
       group: "ПМ-91",
-      groups: [
-        "ПМ-91",
-        "ПМ-72"
-      ],
     }
   }
 
@@ -128,10 +116,14 @@ class Screen1 extends React.Component {
   }
 
   componentDidMount() {
-    const list = Array(15).fill(0).map((item, i) => ({
-      name: ("Student " + i),
+
+  }
+
+  render() {
+    const tableRows = Array(15).fill(0).map((item, i) => ({
+      name: ("Student кншвкнвквнв 2345678923457" + i),
       gender: i % 2 ? "M" : "Ж",
-      dir: i % 3 === 0 ? "Легкая атлетика" : "Н/Д",
+      dir: i % 3 === 0 ? "Легкая атлетика цвуапро 234578увакпрол" : "Н/Д",
       teacher: i % 4 === 0 ? "TheBest" : "Н/Д",
       jump: 0,
       up: 0,
@@ -143,22 +135,31 @@ class Screen1 extends React.Component {
       I: "00.00",
     }));
 
-    const listItems = list.map((listItem, i) =>
-      <TableRowFun hide={this.state.isOnlyNormatives} key={i} value={listItem} />);
+    const years = [2023, 2022]
+    const semesters = ["Осенний", "Весенний"]
+    const faculties = ["ФПМИ", "ФЛА", "АВТФ"];
+    const courses = [1, 2, 3, 4]
+    const groups = ["ПМ-91", "ПМ-72"];
 
-    this.setState({
-      listItems: listItems
-    })
-  }
-
-  render() {
     return (
-      <Paper>
-        <Stack direction="row" spacing={2} alignItems="center">
+      <Paper style={{
+        display: 'flex',
+        height: "100vh",
+        flexDirection: "column"
+      }}>
+        <Stack
+          direction="row"
+          spacing={2}
+          alignItems="center"
+        >
+
+          {/* Только нормативы */}
           <FormControlLabel
             control={<Checkbox onChange={this.isOnlyNormativesChange} />}
             label="Показывать только нормативы"
           />
+
+          {/* Год */}
           <FormControl>
             <InputLabel>Год</InputLabel>
             <Select
@@ -167,11 +168,11 @@ class Screen1 extends React.Component {
               onChange={event => this.yearChange(event.target.value)}
               size="small"
             >
-              <MenuItem value={2023}>2023</MenuItem>
-              <MenuItem value={2022}>2022</MenuItem>
-              <MenuItem value={2021}>2021</MenuItem>
+              {years.map((year) => <MenuItem value={year}>{year}</MenuItem>)}
             </Select>
           </FormControl>
+
+          {/* Семестр */}
           <FormControl>
             <InputLabel>Семестр</InputLabel>
             <Select
@@ -180,18 +181,21 @@ class Screen1 extends React.Component {
               onChange={event => this.semesterChange(event.target.value)}
               size="small"
             >
-              <MenuItem value="Осенний">Осенний</MenuItem>
-              <MenuItem value="Весенний">Весенний</MenuItem>
+              {semesters.map((semester) => <MenuItem value={semester}>{semester}</MenuItem>)}
             </Select>
           </FormControl>
+
+          {/* Факультет */}
           <Autocomplete
             disablePortal
-            options={this.state.faculties}
-            sx={{ width: 200 }}
+            options={faculties}
+            style={{ width: 200 }}
             renderInput={(params) => <TextField {...params} label="Факультет" />}
             onChange={event => this.facultyChange(event.target.value)}
             size="small"
           />
+
+          {/* Курс */}
           <FormControl>
             <InputLabel>Курс</InputLabel>
             <Select
@@ -200,46 +204,49 @@ class Screen1 extends React.Component {
               onChange={event => this.courseChange(event.target.value)}
               size="small"
             >
-              <MenuItem value={1}>1</MenuItem>
-              <MenuItem value={2}>2</MenuItem>
-              <MenuItem value={3}>3</MenuItem>
-              <MenuItem value={4}>4</MenuItem>
+              {courses.map((course) => <MenuItem value={course}>{course}</MenuItem>)}
             </Select>
           </FormControl>
+
+          {/* Группы */}
           <Autocomplete
             disablePortal
-            options={this.state.groups}
+            options={groups}
             sx={{ width: 200 }}
             renderInput={(params) => <TextField {...params} label="Группа" />}
             onChange={event => this.groupChange(event.target.value)}
             size="small"
           />
         </Stack>
-        <TableContainer>
-          <Table>
+
+        {/* Таблица */}
+        <TableContainer
+          style={{ flexGrow: 1 }}
+        >
+          <Table stickyHeader size="small">
             <TableHead>
               <TableRow>
-                <TableCellFun hide={false} className="tableCellRows" rowSpan={2} value="Студент" />
-                <TableCellFun hide={this.state.isOnlyNormatives} className="tableCellRows" rowSpan={2} value="Пол" />
-                <TableCellFun hide={this.state.isOnlyNormatives} className="tableCellRows" rowSpan={2} value="Отделение" />
-                <TableCellFun hide={this.state.isOnlyNormatives} className="tableCellRows" rowSpan={2} value="Преподаватель" />
-                <TableCellFun hide={false} colSpan={5} value="Нормативы" />
-                <TableCellFun hide={false} colSpan={3} value="Измерения" />
+                <RenderTableCell style={{ minWidth: 150 }} hide={false} className="tableCellRows" rowSpan={2} value="Студент" />
+                <RenderTableCell style={{ minWidth: 150 }} hide={this.state.isOnlyNormatives} className="tableCellRows" rowSpan={2} value="Пол" />
+                <RenderTableCell style={{ minWidth: 150 }} hide={this.state.isOnlyNormatives} className="tableCellRows" rowSpan={2} value="Отделение" />
+                <RenderTableCell style={{ minWidth: 150 }} hide={this.state.isOnlyNormatives} className="tableCellRows" rowSpan={2} value="Преподаватель" />
+                <RenderTableCell style={{ minWidth: 150 }} hide={false} colSpan={5} value="Нормативы" />
+                <RenderTableCell style={{ minWidth: 150 }} hide={false} colSpan={3} value="Измерения" />
               </TableRow>
               <TableRow>
-                <TableCellFun value="Прыжок в длину" />
-                <TableCellFun value="Подъем туловища" />
-                <TableCellFun value="Подтягивание" />
-                <TableCellFun value="Гибкость" />
-                <TableCellFun value="Бег" />
-                <TableCellFun value="Р" />
-                <TableCellFun value="В" />
-                <TableCellFun value="И" />
+                <RenderTableCell style={{ top: 37, minWidth: 150 }} value="Прыжок в длину" />
+                <RenderTableCell style={{ top: 37, minWidth: 150 }} value="Подъем туловища" />
+                <RenderTableCell style={{ top: 37, minWidth: 150 }} value="Подтягивание" />
+                <RenderTableCell style={{ top: 37, minWidth: 150 }} value="Гибкость" />
+                <RenderTableCell style={{ top: 37, minWidth: 150 }} value="Бег" />
+                <RenderTableCell style={{ top: 37, minWidth: 150 }} value="Р" />
+                <RenderTableCell style={{ top: 37, minWidth: 150 }} value="В" />
+                <RenderTableCell style={{ top: 37, minWidth: 150 }} value="И" />
               </TableRow>
             </TableHead>
             <TableBody>
-              {this.state.list.map((listItem, i) =>
-                <TableRowFun hide={this.state.isOnlyNormatives} key={i} value={listItem} />)}
+              {tableRows.map((listItem, i) =>
+                <RenderTableRow hide={this.state.isOnlyNormatives} key={i} value={listItem} />)}
             </TableBody>
           </Table>
         </TableContainer>
